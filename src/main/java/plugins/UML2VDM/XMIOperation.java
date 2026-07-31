45a1a68a0dbdef38f19f0906311fca074e810f29
Nick Battle <nick.battle@gmail.com>
2026-07-31 17:32:59 +0100

    Fix "()" parameter lists

--- src/main/java/plugins/UML2VDM/XMIOperation.java
+++ src/main/java/plugins/UML2VDM/XMIOperation.java
@@ -48,7 +48,7 @@ public class XMIOperation {
         else 
         {
             this.shortName = opName + "() == ()";
-            readyArgLine = "";
+            readyArgLine = "()";
         }
 
 
