894fc14ba524e8564a205fb86053f104c50feb3f
Nick Battle <nick.battle@gmail.com>
2026-08-02 17:23:03 +0100

    Tidy XMIOperation

--- src/main/java/plugins/UML2VDM/XMIOperation.java
+++ src/main/java/plugins/UML2VDM/XMIOperation.java
@@ -14,8 +14,6 @@ public class XMIOperation {
     private OpTypes opType;
     private String shortName;
     private String visibility;
-    private Boolean hasShortName;
-    private String[] args;
 
     public XMIOperation(Element aElement)
     {
@@ -53,24 +51,24 @@ public class XMIOperation {
 
 			if (rtype == null) rtype = "()";
 
-			// Turn "nat, char, int" ptypes into "a, b, c" pnames
+			// Turn "nat, char, int" into "a, b, c" and "nat * char * bool"
 			String pnames = "";
 
 			if (!ptypes.isEmpty())
 			{
 				String[] parts = ptypes.split("\\s*,\\s*");
 				char arg = 'a';
-				String sep = "";
-				String sep2 = "";
+				String psep = "";
+				String tsep = "";
 				String types = "";
 
 				for (int a=0; a < parts.length; a++)
 				{
-					pnames = pnames + sep + arg;
-					types = types + sep2 + parts[a];
-					sep = ", ";
-					sep2 = " * ";
-					arg = (char)(arg + 1);
+					pnames = pnames + psep + arg;
+					types  = types  + tsep + parts[a];
+					psep = ", ";
+					tsep = " * ";
+					arg = (char)(arg + 1);	// a, b, c etc...
 				}
 
 				ptypes = types;
@@ -82,129 +80,16 @@ public class XMIOperation {
 
 			if (opType == OpTypes.function)
 			{
-				signature = name + ": " + ptypes + " -> " + rtype;
-				shortName = name + "("  + pnames + ") == is not yet specified;";
+				this.signature = name + ": " + ptypes + " -> " + rtype;
+				this.shortName = name + "("  + pnames + ") == is not yet specified;";
 			}
 			else
 			{
-				signature = name + ": " + ptypes + " ==> " + rtype;
-				shortName = name + "("  + pnames + ") == is not yet specified;";
+				this.signature = name + ": " + ptypes + " ==> " + rtype;
+				this.shortName = name + "("  + pnames + ") == is not yet specified;";
 			}
 		}
-
-/***
-        hasShortName = false;
-        
-        if (aElement.getAttribute("name").contains("«function»"))
-        {
-            this.opType = OpTypes.function;
-            xmiName = remove(xmiName, "«function»");      
-        }	
-        else
-            this.opType = OpTypes.operation;
-
-        String seg1[] = xmiName.split("\\(");
-        String opName = seg1[0];
-        String parameters[] = (seg1[seg1.length - 1]).split("\\)");
-        String readyArgLine;
-
-        if(!(parameters.length == 0))
-        {
-            String[] abreviated = shortNames(parameters);   
-            readyArgLine = vdmArgline();
-
-            String newShortName = opName + "(" + abreviated[0];
-            for(int n = 1; n < abreviated.length ; n++)
-            {
-                newShortName = newShortName + ","  + abreviated[n];
-            }
-            this.shortName = newShortName + ") == ()" ;
-        } 
-
-        else 
-        {
-            this.shortName = opName + "() == ()";
-            readyArgLine = "()";
-        }
-
-
-        String opOut;
-        if (xmiName.contains(":"))
-        {
-            String seg3[] =  xmiName.split(":");
-            opOut = seg3[seg3.length - 1];
-        }
-        else 
-            opOut = " ()";
-
-
-        if(this.opType == OpTypes.operation)
-            this.signature = opName + " : " + readyArgLine + " ==>" + opOut; 
-
-        else
-            this.signature = opName + " : " + readyArgLine + " ->" + opOut;
-        
-        this.visibility = visibility(aElement);
-***/
     }
-
-	private String vdmArgline()
-    {
-        if (!(args == null))
-        {
-            String vdmArgLine = args[0];
-            
-            for(int n = 1 ; n < args.length ; n++)
-            {
-                vdmArgLine = vdmArgLine + " *" + args[n];            
-            }
-            return vdmArgLine;
-        }
-        else return "";
-    }
-
-    private String[] shortNames(String[] s)
-    {
-        if ((!(s.length == 0)))
-        {
-           
-            String argLine = s[0];
-            args = argLine.split(",");
-
-            String [] shortnames = new String[args.length];
-
-            for(int n = 0; n < args.length; n++)     
-            {
-                if(args[n].contains("in ") || args[n].contains(" in "))
-                {
-                    args[n] = args[n].replace("in ", "");
-                    hasShortName = true;
-                }
-                String inSeg1[] = args[n].split(":");
-                
-                if(hasShortName == true)
-                    shortnames[n] = inSeg1[0];
-                else
-                    shortnames[n] = "";
-                
-                hasShortName = false;
-                
-                if (!(inSeg1.length == 0))
-                    args[n] = inSeg1[inSeg1.length - 1];
-                
-            }
-           
-            return shortnames;
-        }
-
-        else return new String[0];
-
-    }
-
-    private String remove(String s, String r)
-	{
-        return s.replace(r, "");
-	}
     
     private String visibility(Element element)
 	{
@@ -239,9 +124,6 @@ public class XMIOperation {
     {
         return opType;
     }
-
-
-    
 }
     
 
